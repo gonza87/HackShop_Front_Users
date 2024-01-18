a12a7d54da19f98a71bc3d1af09b94c469397ebf
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-01-18 14:32:35 -0300

    bannerTarjeta

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,16 @@ function App() {
   return (
    <>
    <Carrusel/>
+   <div className='main'>
+      <div className='container'>
+      <div className='row'>
+        <div className='col-12'>
+          <div className='bannerTarjeta mt-4'> </div>
+        </div>
+      </div>
+    </div>
+   </div>
+   
    </>
    
   )
