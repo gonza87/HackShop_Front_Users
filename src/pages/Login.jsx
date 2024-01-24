0726b34da8255e64c614d23fcdba8c806d38a2b9
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-01-24 10:51:10 -0300

    loguinMejoras

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,34 +8,34 @@ function Login(){
     return(
         <>  
            <div className="container-fluid">
-            <div className="row">
-                <div className="col-6 loginIzquierda">
-                    <p>Hack Shop</p>
+            <div className="row flex-column-reverse flex-md-row">
+                <div className="col-md-6 loginIzquierda">
+                    <p className='textHackshop'>Hack Shop</p>
                     <p>Bienvenido</p>
                     <div className='d-flex align-items-center justify-content-center'>
-                        <Form className='formLogin'>
+                        <Form className='formLogin mt-4'>
                             <Row>
                                 <Col>
                                 <Form.Label className='textCampoLogin'>Email Addres</Form.Label><span className='testRegistro'>No tenés cuenta? <a href="">Registraté acá</a></span>
                                 
-                                <Form.Control type="email" placeholder="Escribí tu email acá" className='uno'/>
+                                <Form.Control type="email" placeholder="Escribí tu email acá"/>
                                 </Col>
                                 
                             </Row>
                             <Row>
                                 <Col>
-                                <Form.Label className='textCampoLogin'>Password</Form.Label>
-                                <Form.Control type='password' placeholder="Contraseña" />
+                                <Form.Label className='textCampoLogin mt-3'>Password</Form.Label>
+                                <Form.Control type='password' placeholder="Contraseña"/>
                                 </Col>
                                 
                             </Row>
-                            <Button type="submit"className='btnLogin'>Iniciar Sesión</Button>
+                            <Button type="submit"className='btnLogin mt-4'>Iniciar Sesión</Button>
                         </Form>
                         
                     </div>
                     
                 </div>
-                <div className="col-6 loginDerecha">
+                <div className="col-md-6 loginDerecha">
                    
                         <img src="/public/logo.png" alt="" className='logoLogin'/>
                     
