d26a551ae37c5946b6372144f75174340bace407
mariosilveira1980 <139134070+mariosilveira1980@users.noreply.github.com>
2024-01-24 19:09:42 -0300

    Componente AddToCart y Description
     Se crea el componente con el boton agregar carrito, ademas se hace responsive el componente Description

--- src/pages/Description.jsx
+++ src/pages/Description.jsx
@@ -1,19 +1,17 @@
-import Dropdown from "react-bootstrap/Dropdown";
-import DropdownButton from "react-bootstrap/DropdownButton";
 import Footer from "../components/Footer";
 import Whatsapp from "../components/Whatsapp";
 import NavbarComponent from "../components/Navbar";
-
-
+import AddToCart from "../components/AddToCart";
 import Card from "react-bootstrap/Card";
+import './description.css';
 
 function Description() {
   return (
     <>
-    <NavbarComponent/>
-      <div className="container containerCategory">
+      <NavbarComponent />
+      <div className="container-fluid containerCategory">
         <div className="row">
-          <div className="col-3 izquierdaCategoria">
+          <div className="col-md-3 izquierdaCategoria">
             <p className="fs-5">CATEGORÍAS DE PRODUCTOS</p>
             <a href="">
               <p>GPU</p>
@@ -34,42 +32,45 @@ function Description() {
               <p>SILLAS GAMER</p>
             </a>
           </div>
-          <div className="col-9">
+          <div className="col-md-9">
             <div className="row">
-              <div className="col-8">
-                <h2>Procesadores</h2>
+              <div className="col-md-8">
+                <h2 className="display-4">Procesadores</h2>
               </div>
-              <div className="col-4">
-              
-              </div>
+              <div className="col-md-4"></div>
             </div>
             <div className="row">
-              <div className="col-12 d-flex">
-                
-                <Card
-                  style={{ width: "100rem", height: "20rem" }}
-                  className="m-1"
-                >
+              <div className="col-md-12 d-flex">
+                <Card style={{ width: "20rem", height: "20rem" }} className="m-1">
                   <Card.Img
                     variant="top"
                     src="/public/procesadores/PROCESADOR-INTEL-I5-12400F-SVIDEO-12VA-1700.jpg"
-                    className="imgListCat"
+                    className="imgListCat img-fluid"
                   />
                 </Card>
-                <div>
-                  <h2>
-                    Intel Core i5 10400f 2.9GHz 12MB LGA1200 10th Gen US$147.50
-                    DISPONIBILIDAD: MÁS DE 5 DISPONIBLES SKU: BX8070110400F
-                    CATEGORÍAS: PROCESADORES, PROCESADORES INTEL
-                  </h2>
+                <div className="description">
+                  <h2 className="display-4">Intel Core i5 12400 Alder Lake 1700</h2>
+                  <span className="price">
+                    <h5  style={{ color: '#ff6200', fontSize: '1.2rem' }}>US$259.99</h5>
+                  </span>
+                  <br />
+                  <span>DISPONIBILIDAD: 2 DISPONIBLES</span>
+                  <br />
+                  <span>SKU: BX8071512400</span>
+                  <br />
+                  <span>CATEGORÍAS: PROCESADORES, PROCESADORES INTEL</span>
+                  <br />
+                  <hr />
+                  <AddToCart />
+                  
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <Whatsapp/>
-        <Footer/>
+      <Whatsapp />
+      <Footer />
     </>
   );
 }
