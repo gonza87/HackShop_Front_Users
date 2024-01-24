d64d31941bf8928425df2dc12ae13f06e759278e
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-01-23 21:54:08 -0300

    logion

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
 import './Login.css';
 
 function Login(){
@@ -11,23 +12,26 @@ function Login(){
                 <div className="col-6 loginIzquierda">
                     <p>Hack Shop</p>
                     <p>Bienvenido</p>
-                    <div className='d-flex justify-content-center'>
-                        <Form>
-                        <Row>
-                            <Col>
-                            <Form.Label>Email Addres</Form.Label>
-                            <Form.Control type="email" placeholder="Escribí tu email acá" className='uno'/>
-                            </Col>
-                            
-                        </Row>
-                        <Row>
-                            <Col>
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type='password' placeholder="contraseña" />
-                            </Col>
-                            
-                        </Row>
-                    </Form>
+                    <div className='d-flex align-items-center justify-content-center'>
+                        <Form className='formLogin'>
+                            <Row>
+                                <Col>
+                                <Form.Label className='textCampoLogin'>Email Addres</Form.Label><span className='testRegistro'>No tenés cuenta? <a href="">Registraté acá</a></span>
+                                
+                                <Form.Control type="email" placeholder="Escribí tu email acá" className='uno'/>
+                                </Col>
+                                
+                            </Row>
+                            <Row>
+                                <Col>
+                                <Form.Label className='textCampoLogin'>Password</Form.Label>
+                                <Form.Control type='password' placeholder="Contraseña" />
+                                </Col>
+                                
+                            </Row>
+                            <Button type="submit"className='btnLogin'>Iniciar Sesión</Button>
+                        </Form>
+                        
                     </div>
                     
                 </div>
