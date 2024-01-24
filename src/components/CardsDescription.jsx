41c00e8d8c9ba3c0d4e980597628ca5dc8c059c5
mariosilveira1980 <139134070+mariosilveira1980@users.noreply.github.com>
2024-01-23 23:20:16 -0300

    about responsive y disenio

--- src/components/CardsDescription.jsx
+++ src/components/CardsDescription.jsx
@@ -1,49 +1,52 @@
+import React from "react";
 import Card from "react-bootstrap/Card";
 
 function CardsDescription() {
   return (
     <div className="container">
-    <div className="row mt-4">
-      <div className="col-3 mx-auto">
+      <div className="row mt-4">
+        <div className="col-md-4 mb-4">
+          <Card style={{ width: "22rem", height: "24rem" }} className="text-center">
+            <Card.Body>
+              <Card.Title>Duración</Card.Title>
+              <Card.Text>
+                El proyecto fue desarrollado en tan sólo 3 semanas, durante noviembre de 2022. El mismo se dividió en sprints (Scrum) de una semana de duración.
+                El proyecto fue desarrollado en tan sólo 3 semanas, durante noviembre de 2022. El mismo se dividió en sprints (Scrum) de una semana de duración.
+                El proyecto fue desarrollado en tan sólo 3 semanas, durante noviembre de 2022. El mismo se dividió en sprints (Scrum) de una semana de duración.
+                El proyecto fue desarrollado en tan sólo 3 semanas, durante noviembre de 2022. El mismo se dividió en sprints (Scrum) de una semana de duración.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
 
-        <Card style={{ width: "22rem", height:"24rem" }} className="text-center">
-          <Card.Body>
-            <Card.Title>Duración</Card.Title>
-            <Card.Text>El proyecto fue desarrollado en tan sólo 3 semanas, durante noviembre de 2022. El mismo se dividió en sprints (Scrum) de una semana de duración.
+        <div className="col-md-4 mb-4">
+          <Card style={{ width: "22rem", height: "24rem" }} className="text-center">
+            <Card.Body>
+              <Card.Title>Tecnologías</Card.Title>
+              <Card.Text>
                 El proyecto fue desarrollado en tan sólo 3 semanas, durante noviembre de 2022. El mismo se dividió en sprints (Scrum) de una semana de duración.
                 El proyecto fue desarrollado en tan sólo 3 semanas, durante noviembre de 2022. El mismo se dividió en sprints (Scrum) de una semana de duración.
                 El proyecto fue desarrollado en tan sólo 3 semanas, durante noviembre de 2022. El mismo se dividió en sprints (Scrum) de una semana de duración.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </div>
-      <div className="col-3 mx-auto">
+                El proyecto fue desarrollado en tan sólo 3 semanas, durante noviembre de 2022. El mismo se dividió en sprints (Scrum) de una semana de duración.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
 
-        <Card style={{ width: "22rem", height:"24rem" }} className="text-center">
-          <Card.Body>
-            <Card.Title>Tecnologías</Card.Title>
-            <Card.Text>El proyecto fue desarrollado en tan sólo 3 semanas, durante noviembre de 2022. El mismo se dividió en sprints (Scrum) de una semana de duración.
+        <div className="col-md-4 mb-4">
+          <Card style={{ width: "22rem", height: "24rem" }} className="text-center">
+            <Card.Body>
+              <Card.Title>División de tareas</Card.Title>
+              <Card.Text>
                 El proyecto fue desarrollado en tan sólo 3 semanas, durante noviembre de 2022. El mismo se dividió en sprints (Scrum) de una semana de duración.
                 El proyecto fue desarrollado en tan sólo 3 semanas, durante noviembre de 2022. El mismo se dividió en sprints (Scrum) de una semana de duración.
                 El proyecto fue desarrollado en tan sólo 3 semanas, durante noviembre de 2022. El mismo se dividió en sprints (Scrum) de una semana de duración.
-            </Card.Text>
-          </Card.Body>
-        </Card>
+                El proyecto fue desarrollado en tan sólo 3 semanas, durante noviembre de 2022. El mismo se dividió en sprints (Scrum) de una semana de duración.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
       </div>
-      <div className="col-3 mx-auto">
-
-        <Card style={{ width: "22rem", height:"24rem" }} className="text-center">
-          <Card.Body>
-            <Card.Title>División de tareas</Card.Title>
-            <Card.Text>El proyecto fue desarrollado en tan sólo 3 semanas, durante noviembre de 2022. El mismo se dividió en sprints (Scrum) de una semana de duración.
-                El proyecto fue desarrollado en tan sólo 3 semanas, durante noviembre de 2022. El mismo se dividió en sprints (Scrum) de una semana de duración.
-                El proyecto fue desarrollado en tan sólo 3 semanas, durante noviembre de 2022. El mismo se dividió en sprints (Scrum) de una semana de duración.
-                El proyecto fue desarrollado en tan sólo 3 semanas, durante noviembre de 2022. El mismo se dividió en sprints (Scrum) de una semana de duración.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </div>
-    </div>
     </div>
   );
 }
--- src/components/CardsPerson.jsx
+++ src/components/CardsPerson.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -14,7 +15,8 @@ function CardsPerson() {
   return (
     <div className="container">
       <div className="row mt-4">
-        <div className="col-4">
+        
+        <div className="col-md-6 mb-4">
           <h5 className="paragraph">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et,
             debitis consequuntur, tempore accusamus excepturi omnis sint
@@ -22,12 +24,13 @@ function CardsPerson() {
             deserunt ipsum eaque, consequuntur culpa quia.
           </h5>
         </div>
-        <div className="col-8 d-flex justify-content-end">
-          <Card style={{ width: "18rem" }}>
+
+        
+        <div className="col-md-6 mb-4">
+          <Card style={{ width: "100%" }}>
             <Card.Img src="/public/Oficina.jpg" style={{ height: "250px" }} />
             <Card.Body>
-              <Card.Title>Santiago Ourthe Cabalé 
-</Card.Title>
+              <Card.Title>Santiago Ourthe Cabalé</Card.Title>
             </Card.Body>
             <ListGroup className="list-group-flush">
               <ListGroup.Item>Junior Full Stack Developer</ListGroup.Item>
@@ -41,22 +44,38 @@ function CardsPerson() {
               <div className="icons">
                 <FontAwesomeIcon
                   className="black-icon"
-                  icon={faSquareFacebook} size="2x"
+                  icon={faSquareFacebook}
+                  size="2x"
                 />
-
                 <a href="#">
-                  <FontAwesomeIcon className="black-icon" icon={faInstagram} size="2x" />
+                  <FontAwesomeIcon
+                    className="black-icon"
+                    icon={faInstagram}
+                    size="2x"
+                  />
                 </a>
                 <a href="#">
-                  <FontAwesomeIcon className="black-icon" icon={faXTwitter} size="2x"/>
+                  <FontAwesomeIcon
+                    className="black-icon"
+                    icon={faXTwitter}
+                    size="2x"
+                  />
                 </a>
                 <a href="#">
-                  <FontAwesomeIcon className="black-iconT" icon={faTiktok}size="2x" />
+                  <FontAwesomeIcon
+                    className="black-iconT"
+                    icon={faTiktok}
+                    size="2x"
+                  />
                 </a>
               </div>
             </Card.Body>
           </Card>
-          <Card style={{ width: "18rem" }}>
+        </div>
+
+        
+        <div className="col-md-6 mb-4">
+          <Card style={{ width: "100%" }}>
             <Card.Img src="/public/601906fb4153a.jpeg" style={{ height: "250px" }} />
             <Card.Body>
               <Card.Title>Gonzalo Viglioni</Card.Title>
@@ -73,22 +92,38 @@ function CardsPerson() {
               <div className="icons">
                 <FontAwesomeIcon
                   className="black-icon"
-                  icon={faSquareFacebook}size="2x"
+                  icon={faSquareFacebook}
+                  size="2x"
                 />
-
                 <a href="#">
-                  <FontAwesomeIcon className="black-icon" icon={faInstagram}size="2x" />
+                  <FontAwesomeIcon
+                    className="black-icon"
+                    icon={faInstagram}
+                    size="2x"
+                  />
                 </a>
                 <a href="#">
-                  <FontAwesomeIcon className="black-icon" icon={faXTwitter}size="2x" />
+                  <FontAwesomeIcon
+                    className="black-icon"
+                    icon={faXTwitter}
+                    size="2x"
+                  />
                 </a>
                 <a href="#">
-                  <FontAwesomeIcon className="black-iconT" icon={faTiktok}size="2x" />
+                  <FontAwesomeIcon
+                    className="black-iconT"
+                    icon={faTiktok}
+                    size="2x"
+                  />
                 </a>
               </div>
             </Card.Body>
           </Card>
-          <Card style={{ width: "18rem" }}>
+        </div>
+
+        
+        <div className="col-md-6 mb-4">
+          <Card style={{ width: "100%" }}>
             <Card.Img src="/public/oficina.webp" style={{ height: "250px" }} />
             <Card.Body>
               <Card.Title>Mario Silveira</Card.Title>
@@ -105,17 +140,29 @@ function CardsPerson() {
               <div className="icons">
                 <FontAwesomeIcon
                   className="black-icon"
-                  icon={faSquareFacebook}size="2x"
+                  icon={faSquareFacebook}
+                  size="2x"
                 />
-
                 <a href="#">
-                  <FontAwesomeIcon className="black-icon" icon={faInstagram}size="2x" />
+                  <FontAwesomeIcon
+                    className="black-icon"
+                    icon={faInstagram}
+                    size="2x"
+                  />
                 </a>
                 <a href="#">
-                  <FontAwesomeIcon className="black-icon" icon={faXTwitter}size="2x" />
+                  <FontAwesomeIcon
+                    className="black-icon"
+                    icon={faXTwitter}
+                    size="2x"
+                  />
                 </a>
                 <a href="#">
-                  <FontAwesomeIcon className="black-iconT" icon={faTiktok}size="2x" />
+                  <FontAwesomeIcon
+                    className="black-iconT"
+                    icon={faTiktok}
+                    size="2x"
+                  />
                 </a>
               </div>
             </Card.Body>
