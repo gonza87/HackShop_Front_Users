d2ca2547d35ae288df9f6ee1e0635179b52f994f
mariosilveira1980 <139134070+mariosilveira1980@users.noreply.github.com>
2024-01-23 23:22:42 -0300

    harcod de carrito info y vista

--- src/components/Carrito.jsx
+++ src/components/Carrito.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Offcanvas from 'react-bootstrap/Offcanvas';
+import { useState } from "react";
+import { ListGroup, Button  } from "react-bootstrap";
+import Offcanvas from "react-bootstrap/Offcanvas";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+
+import Card from "react-bootstrap/Card";
 function Carrito() {
   const [show, setShow] = useState(false);
 
@@ -11,16 +13,38 @@ function Carrito() {
 
   return (
     <>
-    
-      <FontAwesomeIcon className='ms-2' icon={faShoppingCart} style={{ cursor: 'pointer' }} onClick={handleShow}/>
+      <FontAwesomeIcon
+        className="ms-2"
+        icon={faShoppingCart}
+        style={{ cursor: "pointer" }}
+        onClick={handleShow}
+      />
 
       <Offcanvas show={show} onHide={handleClose} placement="end">
         <Offcanvas.Header closeButton>
           <Offcanvas.Title>Mi Carrito</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          Some text as placeholder. In real life you can have the elements you
-          have chosen. Like, text, images, lists, etc.
+          <div>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="/public/procesadores/PROCESADOR-INTEL-I5-12400F-SVIDEO-12VA-1700.jpg" />
+              <Card.Body>
+                
+                
+              </Card.Body>
+              <ListGroup className="list-group-flush">
+                <ListGroup.Item>1 ITEM</ListGroup.Item>
+                <ListGroup.Item>Intel Core i5 10400f 2.9GHz 12MB LGA1200 10th Gen</ListGroup.Item>
+                <ListGroup.Item>2 × US$147.50</ListGroup.Item>
+                <ListGroup.Item>SUBTOTAL: US$295.00</ListGroup.Item>
+                <Card.Link href="#"><h4 className="verCarrito">Ver carrito</h4></Card.Link>
+              </ListGroup>
+              <Card.Body>
+                <Button variant="primary">FINALIZAR COMPRA</Button>
+                
+              </Card.Body>
+            </Card>
+          </div>
         </Offcanvas.Body>
       </Offcanvas>
     </>
