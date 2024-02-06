0a7e30d47720bd60d5600d292529804c3d3fcfe5
mariosilveira1980 <139134070+mariosilveira1980@users.noreply.github.com>
2024-02-06 14:00:26 -0300

    se arregla carrito en nav alineado

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,65 +1,76 @@
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import {  Link } from 'react-router-dom';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from '@fortawesome/free-regular-svg-icons';
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+
 import Carrito from './Carrito';
 import NavbarAside from './NavbarAside';
 import 'animate.css';
 import './nav.css';
-
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
-
-
 function NavbarComponent() {
+  const [searchQuery, setSearchQuery] = useState("");  // Nuevo estado para la búsqueda
+  const navigate = useNavigate();  // Hook de React Router para la navegación
+
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    // Navega a la página de búsqueda con el query en la URL
+    navigate(`/search?q=${searchQuery}`);
+  };
+
   return (
     <Navbar collapseOnSelect expand="md" className='mianav' fixed="top">
-    <Container>
-      <Navbar.Brand><Link to="/" ><img className='logo animate__animated animate__flip animate__slow' src="/logo3.png" alt="logo" /></Link></Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" style={{ borderColor: 'white' }}><FontAwesomeIcon icon={faBars} style={{ color: 'white' }} /> </Navbar.Toggle>
-      <Navbar.Collapse  id="responsive-navbar-nav">
-            <div className='row container'>
-              <div className='col-md-10 '>
-                <Form className="d-flex">
+      <Container>
+        <Navbar.Brand><Link to="/"><img className='logo animate__animated animate__flip animate__slow' src="/logo3.png" alt="logo" /></Link></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" style={{ borderColor: 'white' }}>
+          <FontAwesomeIcon icon={faBars} style={{ color: 'white' }} />
+        </Navbar.Toggle>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <div className='row container'>
+            <div className='col-md-10 '>
+              <Form onSubmit={handleSearchSubmit} className="d-flex">
                 <Form.Control
                   type="search"
                   placeholder="Search"
                   className="me-2"
                   aria-label="Search"
+                  value={searchQuery}
+                  onChange={handleSearchChange}
+                  
                 />
-                <Button variant="light">Search</Button>{' '}
+                <Button variant="light" type="submit">Search</Button>{' '}
               </Form>
-              
-              </div>
-            
-              <div className='col-md-2 columna-b d-flex'>
-                  <div className='d-flex align-items-end contenedorbtnlogin'>
-            <Link to="/login" className='d-flex align-items-center' style={{ textDecoration: 'none' }}><FontAwesomeIcon icon={faUser} size='xl'  style={{color: "#ffffff",}}/>
-            <span className="ms-2" id='btnIngresa'>Ingresá</span></Link>
-          </div>
-          <div className='contenedorcarro'><Carrito/></div>
-              </div>
-              <div className='col-lg-12 columna-a'>
-                <Nav className="me-auto d-flex ">
-                
-                <div className='contenedorbtncategorias'><NavbarAside/></div>
-                <div className='contenedorbtnTodos'><Link to="/store" style={{ textDecoration: 'none' }}><span style={{color: "#ffffff",}}>TODOS</span></Link></div>
-                </Nav> 
-                
-              </div>
             </div>
-            
-        
-      </Navbar.Collapse>
-      
-    </Container>
-  </Navbar>
+            <div className='col-md-2 columna-b d-flex'>
+              <div className='d-flex align-items-end contenedorbtnlogin'>
+                <Link to="/login" className='d-flex align-items-center' style={{ textDecoration: 'none', paddingLeft:"46px" }}>
+                  <FontAwesomeIcon icon={faUser} size='xl' style={{ color: "#ffffff", }} />
+                  <span className="ms-2" id='btnIngresa'>Ingresá</span>
+                </Link>
+              </div>
+              <div className='contenedorcarro' ><Carrito /></div>
+            </div>
+            <div className='col-lg-12 columna-a'>
+              <Nav className="me-auto d-flex ">
+                <div className='contenedorbtncategorias'><NavbarAside /></div>
+                <div className='contenedorbtnTodos'><Link to="/store" style={{ textDecoration: 'none' }}><span style={{ color: "#ffffff", }}>TODOS</span></Link></div>
+              </Nav>
+            </div>
+          </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
