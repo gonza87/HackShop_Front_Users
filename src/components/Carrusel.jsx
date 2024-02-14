54804897483678b93fa4aae9906386458d414bf8
mariosilveira1980 <139134070+mariosilveira1980@users.noreply.github.com>
2024-02-14 16:12:19 -0300

    Se aplica skeleton en la home
    skeleton en carrusel, categorias destacadas y carrusel destacados

--- src/components/Carrusel.jsx
+++ src/components/Carrusel.jsx
@@ -1,4 +1,4 @@
-import Carousel from 'react-bootstrap/Carousel';
+/* import Carousel from 'react-bootstrap/Carousel';
 import "./carrusel.css";
 
 function Carrusel(){
@@ -101,5 +101,75 @@ function Carrusel(){
         </>
     )
 };
+
+export default Carrusel; */
+
+import Carousel from "react-bootstrap/Carousel";
+import { useState, useEffect } from "react";
+import "react-loading-skeleton/dist/skeleton.css";
+import Skeleton from "react-loading-skeleton";
+
+import "./carrusel.css";
+
+function Carrusel() {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
+  return (
+    <>
+      <div className="msjCarrusel">
+        <p>ELEVA TU JUEGO, POTENCIA TU EXPERIENCIA</p> 
+      </div>
+      <div>
+        {isLoading === false ? (
+          <Carousel>
+            <Carousel.Item interval={5000}>
+              <img
+                className="d-block imgCarrusel"
+                src="/public/01.jpg"
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h5>First slide label</h5>
+                <p>
+                  Nulla vitae elit libero, a pharetra augue mollis interdum.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item interval={5000}>
+              <img
+                className="d-block imgCarrusel"
+                src="/public/02.jpg"
+                alt="Second slide"
+              />
+              <Carousel.Caption>
+                <h5>Second slide label</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item interval={5000}>
+              <img
+                className="d-block imgCarrusel"
+                src="/public/03.jpg"
+                alt="Third slide"
+              />
+              <Carousel.Caption>
+                <h5>Third slide label</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        ) : (
+          <Skeleton height={500} />
+        )}
+      </div>
+    </>
+  );
+}
 
 export default Carrusel;
--- src/components/CategoriasDestacadas.jsx
+++ src/components/CategoriasDestacadas.jsx
@@ -1,49 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import {  Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Skeleton from "react-loading-skeleton";
 
 import './CategoriasDestacadas.css';
 
-function CategoriasDestacadas()  {
-  return (
-<>
+function CategoriasDestacadas() {
+  const [isLoading, setIsLoading] = useState(true);
 
-    <div className='catDestacadas'>
+  useEffect(() => {
+    // Simulación de carga de datos
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
+  return (
+    <>
+      <div className='catDestacadas'>
         <p>CATEGORIAS DESTACADAS</p>
-    </div>
-  <div className="d-flex justify-content-between flex-wrap">
-        <Link to="/category/MONITORES">
-            <Card className="cardCatDestacadas" style={{ width: '13rem' }}>
-                <Card.Img variant="top" src="catMonitores.jpg" />    
-            </Card>
-        </Link>
-        <Link to="/category/GPU">
-            <Card className="cardCatDestacadas" style={{ width: '13rem' }}>
-                <Card.Img variant="top" src="catGpu.jpg" />    
-            </Card>
-        </Link>
-         <Link to="/category/NOTEBOOKS">
-            <Card className="cardCatDestacadas" style={{ width: '13rem' }}>
-            <Card.Img variant="top" src="catNotebooks.jpg" />    
-        </Card>
-         </Link>
-         <Link to="/category/PCS ARMADAS">
-        <Card className="cardCatDestacadas" style={{ width: '13rem' }}>
-            <Card.Img variant="top" src="catPc.jpg" />    
-        </Card>
-        </Link>
-        <Link to="/category/SILLAS GAMER">
-        <Card className="cardCatDestacadas" style={{ width: '13rem' }}>
-            <Card.Img variant="top" src="catSillas.jpg" />    
-        </Card>
-        </Link>
-    </div>
-    
-</>
-   
-    
+      </div>
+      <div className="d-flex justify-content-between flex-wrap">
+        {isLoading ? (
+          <>
+            {Array(5).fill(undefined).map((_, index) => (
+              <div className="col-2" key={index}>
+                <Card  style={{ width: '13rem' }}>
+                  <Skeleton height={200} />
+                </Card>
+              </div>
+            ))}
+          </>
+        ) : (
+          <>
+            <Link to="/category/MONITORES">
+              <Card className="cardCatDestacadas" style={{ width: '13rem' }}>
+                <Card.Img variant="top" src="catMonitores.jpg" />
+              </Card>
+            </Link>
+            <Link to="/category/GPU">
+              <Card className="cardCatDestacadas" style={{ width: '13rem' }}>
+                <Card.Img variant="top" src="catGpu.jpg" />
+              </Card>
+            </Link>
+            <Link to="/category/NOTEBOOKS">
+              <Card className="cardCatDestacadas" style={{ width: '13rem' }}>
+                <Card.Img variant="top" src="catNotebooks.jpg" />
+              </Card>
+            </Link>
+            <Link to="/category/PCS ARMADAS">
+              <Card className="cardCatDestacadas" style={{ width: '13rem' }}>
+                <Card.Img variant="top" src="catPc.jpg" />
+              </Card>
+            </Link>
+            <Link to="/category/SILLAS GAMER">
+              <Card className="cardCatDestacadas" style={{ width: '13rem' }}>
+                <Card.Img variant="top" src="catSillas.jpg" />
+              </Card>
+            </Link>
+          </>
+        )}
+      </div>
+    </>
   );
-};
+}
 
 export default CategoriasDestacadas;
+
