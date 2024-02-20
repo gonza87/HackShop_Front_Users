3c8b40252e003d280dd1c99421f16fefbbef6192
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2024-02-20 11:15:18 -0300

    sweetTodos

--- src/pages/StoreProducts.jsx
+++ src/pages/StoreProducts.jsx
@@ -47,20 +47,43 @@ function StoreProducts() {
       };
       setSelectedQuantities(updatedQuantities);
       dispatch(addToCart({ ...product, quantity: currentQuantity + 1 }));
-      Swal.fire({
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 1500,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        },
+      });
+      Toast.fire({
         icon: "success",
         title: "Producto agregado exitosamente",
-        showConfirmButton: false,
-        timer: 1500,
-        timerProgressBar: true,
+        customClass: {
+          container: "my-custom-class",
+        },
       });
     } else {
-      Swal.fire({
-        icon: "error",
-        title: "Stock no disponible",
+      console.log("No hay suficiente stock para la cantidad seleccionada");
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
         showConfirmButton: false,
         timer: 1500,
         timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        },
+      });
+      Toast.fire({
+        icon: "error",
+        title: "Stock no disponible",
+        customClass: {
+          container: "my-custom-class",
+        },
       });
     }
   };
