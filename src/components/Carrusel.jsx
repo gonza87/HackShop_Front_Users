502ad42ddf20cf4039610c0410b1917662cd812c
mariosilveira1980 <139134070+mariosilveira1980@users.noreply.github.com>
2024-02-27 20:33:50 -0300

    nuevo css y jsx de carrusel

---
+++ src/components/Carrusel.jsx
@@ -0,0 +1,53 @@
+import Carousel from "react-bootstrap/Carousel";
+import "./Carrusel.css";
+/* cambiamos css */
+
+function Carrusel() {
+  return (
+    <>
+      <div className="msjCarrusel">
+        <p>ELEVA TU JUEGO, POTENCIA TU EXPERIENCIA</p>
+      </div>
+      <div>
+        <Carousel>
+          <Carousel.Item interval={5000}>
+            <img
+              className="d-block imgCarrusel"
+              src="/public/01.jpg"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h5>First slide label</h5>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={5000}>
+            <img
+              className="d-block imgCarrusel"
+              src="/public/02.jpg"
+              alt="Second slide"
+            />
+            <Carousel.Caption>
+              <h5>Second slide label</h5>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+
+          <Carousel.Item interval={5000}>
+            <img
+              className="d-block imgCarrusel"
+              src="/public/03.jpg"
+              alt="Third slide"
+            />
+            <Carousel.Caption>
+              <h5>Third slide label</h5>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
+    </>
+  );
+}
+
+export default Carrusel;
